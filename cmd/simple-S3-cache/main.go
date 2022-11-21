d6f81a0483aefe95092a2c27da9dce1a2ab7fed5
Mia <i5heu@i5h.eu>
2022-11-21 13:56:43 +0100

    implement single part range requests and HEAD requests

--- cmd/simple-S3-cache/main.go
+++ cmd/simple-S3-cache/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/i5heu/simple-S3-cache/internal/config"
@@ -12,6 +13,7 @@ import (
 	"github.com/i5heu/simple-S3-cache/internal/ramCache"
 	"github.com/i5heu/simple-S3-cache/internal/storageCache"
 
+	"github.com/gotd/contrib/http_range"
 	"github.com/valyala/fasthttp"
 )
 
@@ -53,40 +55,115 @@ func (h *Handler) handler(ctx *fasthttp.RequestCtx) {
 	}()
 
 	ctx.Response.Header.Set("Access-Control-Allow-Origin", h.conf.CORSDomain)
-	ctx.Response.Header.Set("Access-Control-Allow-Methods", "GET")
+	ctx.Response.Header.Set("Access-Control-Allow-Methods", "GET, HEAD")
 	ctx.Response.Header.Set("Cache-Control", "max-age=31536000")
+	ctx.Response.Header.Set("Accept-Ranges", "bytes")
+
+	dataResult := h.getData(ctx)
+	if dataResult.Error != nil {
+		ctx.Response.SetStatusCode(500)
+		ctx.Response.SetBodyString(dataResult.Error.Error())
+		return
+	}
+	size = dataResult.Size
+	cached = dataResult.Cached
+
+	if ctx.Request.Header.Peek("Range") != nil {
+		// parse the range header
+		ranges, err := http_range.ParseRange(string(ctx.Request.Header.Peek("Range")), int64(size))
+		if err != nil {
+			ctx.Response.SetStatusCode(416)
+			return
+		}
+		// we only support one range
+		if len(ranges) > 1 {
+			ctx.Response.SetStatusCode(416)
+			return
+		}
+		// HTTP 206 (Partial Content)
+		ctx.Response.SetStatusCode(206)
+		ctx.Response.Header.Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", ranges[0].Start, ranges[0].Start+ranges[0].Length, size))
+		ctx.Response.Header.Set("Content-Length", strconv.FormatUint(uint64(ranges[0].Length), 10))
+		ctx.Response.Header.Set("Content-Type", dataResult.MIME)
+	} else {
+		ctx.Response.Header.Set("Content-Length", strconv.FormatUint(uint64(size), 10))
+		ctx.Response.Header.Set("Content-Type", dataResult.MIME)
+	}
+
+	if ctx.IsGet() {
+		if ctx.Request.Header.Peek("Range") != nil {
+			// parse the range header
+			ranges, err := http_range.ParseRange(string(ctx.Request.Header.Peek("Range")), int64(size))
+			if err != nil {
+				ctx.Response.SetStatusCode(416)
+				return
+			}
+
+			ctx.Response.SetBody(dataResult.Data[ranges[0].Start : ranges[0].Start+ranges[0].Length])
+		} else {
+			ctx.Response.SetBody(dataResult.Data)
+		}
+
+	}
+}
+
+// size and error
+type DataStoreResult struct {
+	Data   []byte
+	MIME   string
+	Size   uint
+	Cached bool
+	Error  error
+}
+
+func (h *Handler) getData(ctx *fasthttp.RequestCtx) DataStoreResult {
 
 	url := config.GetCompleteURL(h.conf, string(ctx.Path()))
 	cachedData, mime := h.dataStoreRAM.GetCacheData(url)
 	if cachedData != nil {
-		cached = true
-		size = uint(len(cachedData))
-		ctx.Response.Header.Set("Content-Type", mime)
-		ctx.Response.SetBody(cachedData)
-		return
+		return DataStoreResult{
+			Data:   cachedData,
+			MIME:   mime,
+			Size:   uint(len(cachedData)),
+			Cached: true,
+			Error:  nil,
+		}
 	}
 
 	cachedData, mime = h.dataStoreStorage.GetCacheData(url)
 	if cachedData != nil {
-		cached = true
-		h.dataStoreRAM.CacheData(url, cachedData, mime)
-		size = uint(len(cachedData))
-		ctx.Response.Header.Set("Content-Type", mime)
-		ctx.Response.SetBody(cachedData)
-		return
+		return DataStoreResult{
+			Data:   cachedData,
+			MIME:   mime,
+			Size:   uint(len(cachedData)),
+			Cached: true,
+			Error:  nil,
+		}
 	}
 
 	res, err := http.Get(url)
+	defer res.Body.Close()
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
 		ctx.Response.SetStatusCode(500)
-		return
+		return DataStoreResult{
+			Data:   nil,
+			MIME:   "",
+			Size:   0,
+			Cached: false,
+			Error:  err,
+		}
 	}
-	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
 		ctx.Response.SetStatusCode(res.StatusCode)
-		return
+		return DataStoreResult{
+			Data:   nil,
+			MIME:   "",
+			Size:   0,
+			Cached: false,
+			Error:  err,
+		}
 	}
 
 	bytes, err := ioutil.ReadAll(res.Body)
@@ -94,10 +171,15 @@ func (h *Handler) handler(ctx *fasthttp.RequestCtx) {
 		fmt.Println("error reading response body: ", err)
 	}
 
-	size = uint(len(bytes))
 	sanitizedMime := helper.SanitizeMimeType(res.Header.Get("Content-Type"))
 	h.dataStoreRAM.CacheData(url, bytes, sanitizedMime)
 	h.dataStoreStorage.CacheData(url, bytes, sanitizedMime)
-	ctx.Response.Header.Set("Content-Type", sanitizedMime)
-	ctx.Response.SetBody(bytes)
+
+	return DataStoreResult{
+		Data:   bytes,
+		MIME:   sanitizedMime,
+		Size:   uint(len(bytes)),
+		Cached: false,
+		Error:  nil,
+	}
 }
