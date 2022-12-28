4fa7d18b75915eac894082ebdcaed2e349c133e3
Mia <i5heu@i5h.eu>
2022-12-28 21:38:38 +0100

    latency dump

--- cmd/dump/dump.go
+++ cmd/dump/dump.go
@@ -11,13 +11,22 @@ import (
 )
 
 var (
-	device       string = "en0"
+	device       string = "eth0"
 	snapshot_len int32  = 1024
 	promiscuous  bool   = false
 	err          error
-	timeout      time.Duration = 30 * time.Second
+	timeout      time.Duration = 100 * time.Millisecond
 	handle       *pcap.Handle
 )
+
+type SyncAckPack struct {
+	Time            time.Time
+	SourceIP        string
+	DestinationIP   string
+	SourcePort      string
+	DestinationPort string
+	AckSeq          uint32
+}
 
 func main() {
 	// Open device
@@ -27,7 +36,7 @@ func main() {
 	}
 	defer handle.Close()
 
-	var filter string = "tcp port 443"
+	var filter string = "tcp port 80"
 	err = handle.SetDirection(pcap.DirectionInOut)
 	if err != nil {
 		log.Fatal(err)
@@ -38,14 +47,54 @@ func main() {
 	}
 	fmt.Println("Only capturing " + filter + " packets.")
 
+	var session = map[uint32]map[string]SyncAckPack{}
+
 	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		// Process packet here
+		iptype := packet.NetworkLayer().LayerType()
+
+		var srcIP, dstIP string
+
+		if iptype == layers.LayerTypeIPv4 {
+			ip := packet.NetworkLayer().(*layers.IPv4)
+			srcIP = ip.SrcIP.String()
+			dstIP = ip.DstIP.String()
+		} else if iptype == layers.LayerTypeIPv6 {
+			ip := packet.NetworkLayer().(*layers.IPv6)
+			srcIP = ip.SrcIP.String()
+			dstIP = ip.DstIP.String()
+		}
+
 		tcp := packet.TransportLayer().(*layers.TCP)
 
-		if tcp.SYN || (tcp.ACK) {
-			fmt.Println(packet.Metadata().Timestamp, tcp.SrcPort, tcp.DstPort, tcp.SYN, tcp.ACK, tcp.Ack, tcp.FIN, tcp.Seq)
+		//print the map
+		fmt.Println(session)
+
+		if tcp.SYN && tcp.ACK {
+			if _, ok := session[tcp.Ack]; !ok {
+				session[tcp.Ack] = map[string]SyncAckPack{}
+			}
+
+			session[tcp.Ack][srcIP] = SyncAckPack{
+				Time:            packet.Metadata().Timestamp,
+				SourceIP:        srcIP,
+				DestinationIP:   dstIP,
+				SourcePort:      tcp.SrcPort.String(),
+				DestinationPort: tcp.DstPort.String(),
+				AckSeq:          tcp.Ack,
+			}
+		} else if tcp.ACK {
+			if _, ok := session[tcp.Seq]; ok {
+				if _, ok := session[tcp.Seq][dstIP]; ok {
+					syncack := session[tcp.Seq][dstIP]
+
+					fmt.Println("Time:", packet.Metadata().Timestamp.Sub(syncack.Time), "SourceIP:", syncack.SourceIP, "DestinationIP:", syncack.DestinationIP, "SourcePort:", syncack.SourcePort, "DestinationPort:", syncack.DestinationPort, "AckSeq:", syncack.AckSeq)
+
+					delete(session[tcp.Seq], dstIP)
+				}
+			}
 		}
 
 	}
